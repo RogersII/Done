6ebed74be719486e4f58753c30b01c980e5dcd04
RogersII <rogers0809656@gmail.com>
2013-10-28 15:07:30 +0800

    make a new calculator

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -8,6 +8,53 @@
 
 #import <UIKit/UIKit.h>
 
+int Method;
+int SelectNumber = 0;
+float RunningTotal = 0;
+
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *screenLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *zeroButton;
+@property (weak, nonatomic) IBOutlet UIButton *oneButton;
+@property (weak, nonatomic) IBOutlet UIButton *twoButton;
+@property (weak, nonatomic) IBOutlet UIButton *threeButton;
+@property (weak, nonatomic) IBOutlet UIButton *fourButton;
+@property (weak, nonatomic) IBOutlet UIButton *fiveButton;
+@property (weak, nonatomic) IBOutlet UIButton *sixButton;
+@property (weak, nonatomic) IBOutlet UIButton *sevenButton;
+@property (weak, nonatomic) IBOutlet UIButton *eightButton;
+@property (weak, nonatomic) IBOutlet UIButton *nineButton;
+@property (weak, nonatomic) IBOutlet UIButton *acButton;
+@property (weak, nonatomic) IBOutlet UIButton *equalButton;
+@property (weak, nonatomic) IBOutlet UIButton *plusButton;
+@property (weak, nonatomic) IBOutlet UIButton *subtractButton;
+@property (weak, nonatomic) IBOutlet UIButton *timesButton;
+@property (weak, nonatomic) IBOutlet UIButton *divideButton;
+
+
+
+
+
+-(IBAction)Number1:(id)sender;
+-(IBAction)Number2:(id)sender;
+-(IBAction)Number3:(id)sender;
+-(IBAction)Number4:(id)sender;
+-(IBAction)Number5:(id)sender;
+-(IBAction)Number6:(id)sender;
+-(IBAction)Number7:(id)sender;
+-(IBAction)Number8:(id)sender;
+-(IBAction)Number9:(id)sender;
+-(IBAction)Number0:(id)sender;
+
+-(IBAction)Times:(id)sender;
+-(IBAction)Divide:(id)sender;
+-(IBAction)Subtract:(id)sender;
+-(IBAction)Plus:(id)sender;
+-(IBAction)Equals:(id)sender;
+-(IBAction)Allclear:(id)sender;
+
 @end
+
+
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -14,6 +14,203 @@
 
 @implementation ViewController
 
+@synthesize screenLabel = _screenLabel;
+
+-(IBAction)Number1:(id)sender;{
+    SelectNumber = SelectNumber * 10;  // so SelectNumber is default to be 0? What's the default?
+    SelectNumber = SelectNumber + 1;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number2:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 2;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number3:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 3;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number4:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 4;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number5:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 5;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number6:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 6;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number7:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 7;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number8:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 8;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number9:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 9;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+-(IBAction)Number0:(id)sender;{
+    SelectNumber = SelectNumber * 10;
+    SelectNumber = SelectNumber + 0;
+    self.screenLabel.text = [NSString stringWithFormat:@"%i",SelectNumber];
+}
+
+-(IBAction)Times:(id)sender;{
+    
+    if(RunningTotal == 0){
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    Method = 1;
+    SelectNumber = 0;
+}
+-(IBAction)Divide:(id)sender;{
+    
+    if(RunningTotal == 0){
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+
+    Method = 2;
+    SelectNumber = 0;
+}
+-(IBAction)Subtract:(id)sender;{
+    
+    if(RunningTotal == 0){
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+
+    Method = 3;
+    SelectNumber = 0;
+}
+-(IBAction)Plus:(id)sender;{
+    
+    if(RunningTotal == 0){
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+
+    Method = 4;
+    SelectNumber = 0;
+}
+-(IBAction)Equals:(id)sender;{
+    
+    if(RunningTotal == 0){
+        RunningTotal = SelectNumber;
+    }
+    else{
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * SelectNumber;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / SelectNumber;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - SelectNumber;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + SelectNumber;
+                break;
+            default:
+                break;
+        }
+    }
+    Method = 0;
+    SelectNumber = 0;
+    self.screenLabel.text = [NSString stringWithFormat:@"%.2f",RunningTotal];
+    RunningTotal = 0;
+
+}
+-(IBAction)Allclear:(id)sender;{
+    Method = 0;
+    SelectNumber = 0;
+    RunningTotal = 0;
+    self.screenLabel.text = [NSString stringWithFormat:@"0"];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
